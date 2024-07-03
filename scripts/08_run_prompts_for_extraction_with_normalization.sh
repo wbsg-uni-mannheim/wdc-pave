3dc3ce8f89a12abfdbcb8cf3c1c2aa835870f328
abrinkmann <alex.brinkmann@informatik.uni-mannheim.de>
2024-07-03 14:16:31 +0200

    Correct directory in scripts

--- scripts/08_run_prompts_for_extraction_with_normalization.sh
+++ scripts/08_run_prompts_for_extraction_with_normalization.sh
@@ -24,7 +24,7 @@ for dataset in "${datasets[@]}"; do
 
         for no_example_values in "${example_values[@]}"; do
             # Run Zero-Shot with Descriptions and Correspondences for each no_example_values
-            python3 prompts/08_normalization/8_zero_shot_normalization_with_correspondence_examples.py --dataset $dataset --model $model --title $title --description $description --train_percentage $train_percentage  --normalization_params "$normalization_params" --schema_type $schema_type --normalized_only $normalized_only --no_example_values $no_example_values --verbose $verbose
+            python3 prompts/08_extraction_with_normalization/8_zero_shot_normalization_with_correspondence_examples.py --dataset $dataset --model $model --title $title --description $description --train_percentage $train_percentage  --normalization_params "$normalization_params" --schema_type $schema_type --normalized_only $normalized_only --no_example_values $no_example_values --verbose $verbose
         done
     done
 done
@@ -45,7 +45,7 @@ for dataset in "${datasets[@]}"; do
         for shots in "${n_shots[@]}"; do
             for no_example_values in "${n_example_values[@]}"; do
             # In-Context Demonstrations with descriptions and example values
-            python3 prompts/08_normalization/8_few_shot_normalization_description_with_example_correspondences.py --dataset $dataset --model $model --title $title --description $description --train_percentage $train_percentage  --normalization_params "$normalization_params" --normalized_only $normalized_only --shots $shots --example_selector $example_selector --force_from_different_website $force_from_different_website --verbose $verbose --no_example_values $no_example_values --replace_example_values $replace_example_values --schema_type $schema_type
+            python3 prompts/08_extraction_with_normalization/8_few_shot_normalization_description_with_example_correspondences.py --dataset $dataset --model $model --title $title --description $description --train_percentage $train_percentage  --normalization_params "$normalization_params" --normalized_only $normalized_only --shots $shots --example_selector $example_selector --force_from_different_website $force_from_different_website --verbose $verbose --no_example_values $no_example_values --replace_example_values $replace_example_values --schema_type $schema_type
             done
         done
     done
